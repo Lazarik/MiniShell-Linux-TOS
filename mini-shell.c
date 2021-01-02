54fdfb003fa4aadaef8ec914d91953025d2b160e
Sylvain <lazarik@udona.fr>
2021-01-02 19:30:33 +0100

    Initial

---
+++ mini-shell.c
@@ -0,0 +1,15 @@
+/*
+TP: Création d'un Mini Shell pour Linux
+Etudiants: Sylvain MAAMERI, Mathieu LOURENCO, Swan GONCALVES
+Année: 2020-2021 - 2A2
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc,char ** argv) {
+    
+
+    return 0;
+}
