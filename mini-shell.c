d206c8e24b0278743bc4dece788370bbf7ef3d7d
Sylvain <lazarik@udona.fr>
2021-01-02 21:08:18 +0100

    Ajout boucle + commande exit

--- mini-shell.c
+++ mini-shell.c
@@ -8,8 +8,29 @@ Année: 2020-2021 - 2A2
 #include <stdlib.h>
 #include <string.h>
 
+#define INPUT_LENGTH 50
+
 int main(int argc,char ** argv) {
-    
+
+    int miniShell;
+    char input[INPUT_LENGTH];
+
+    miniShell = 1; 
+
+    /*Message de bienvnue dans le Mini Shell*/
+    printf("\nBienvenue dans ce Mini Shell Linux !");
+
+    /*
+    Boucle principale
+    s'execute tant que l'utilisateur ne saisie pas la commande exit
+    */
+    while(miniShell != 0){
+        printf("\nTOS-Shell >");
+        fgets(input,INPUT_LENGTH,stdin);
+
+        /*Vérification si la commande exit est saisie*/
+        miniShell = strcmp(input,"exit\n\0");
+    }
 
     return 0;
 }
